9e2219036dfa0a5d35313fa80216861deb554932
casa98 <cagudeloa@uninorte.edu.co>
2020-10-23 20:45:27 -0500

    Start Ninja-Card project

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(NinjaCard());
 }
 
-class MyApp extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
-            'Hello, Flutter World',
-          ),
-          centerTitle: true,
-        ),
-        body: Row(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Image.asset('assets/lds.jpg'),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 0.0, vertical: 30.0),
-                color: Colors.yellowAccent,
-                child: Text(
-                  '2 portions',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 0.0, vertical: 30.0),
-                color: Colors.amber,
-                child: Text(
-                  '1 portion',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('TAP'),
+          title: Text('Ninja ID Card'),
         ),
       ),
     );
